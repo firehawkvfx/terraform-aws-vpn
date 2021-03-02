a4775a97c063e39ad5bc2a4b22dba0f4de8b5063
Andrew Graham <andrew@firehawkvfx.com>
2021-03-02 16:33:35 +1030

    update missing vars

--- variables.tf
+++ variables.tf
@@ -21,10 +21,21 @@ variable "openvpn_server_ami" {
   type = string
 }
 
+variable "environment" {
+  description = "The environment.  eg: dev/prod"
+  type        = string
+}
+
 variable "resourcetier" {
-  description = "The default resource tier - dev/green/blue/main"
+  description = "The resource tier uniquely defining the deployment area.  eg: dev/green/blue/main"
+  type        = string
+}
+variable "pipelineid" {
+  description = "The pipelineid uniquely defining the deployment instance if using CI.  eg: dev/green/blue/main"
+  type        = string
 }
 
 variable "deployer_ip_cidr" {
   description = "The IP enabled for SSH access to the vpn access server"
 }
+
