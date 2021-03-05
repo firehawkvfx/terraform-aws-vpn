96bdee0f2246d9f0f9aa5acf6f28375e2979935c
Andrew Graham <andrew@firehawkvfx.com>
2021-03-05 22:30:07 +1030

    provide vars for vpn

--- variables.tf
+++ variables.tf
@@ -52,3 +52,18 @@ variable "consul_cluster_name" {
   description = "What to name the Consul server cluster and all of its associated resources"
   type        = string
 }
+
+variable "vpn_cidr" {
+  description = "The CIDR range that the vpn will assign using DHCP.  These are virtual addresses for routing traffic."
+  type        = string
+}
+
+variable "onsite_private_subnet_cidr" {
+  description = "The subnet CIDR Range of your onsite private subnet. This is also the subnet where your VPN client resides in. eg: 192.168.1.0/24"
+  type        = string
+}
+
+variable "onsite_public_ip" {
+  description = "The public ip address of your onsite location to enable access to security groups and openVPN."
+  type = string
+}
