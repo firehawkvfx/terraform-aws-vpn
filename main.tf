b6c0d5ab1b170dec8ff5bc1f7855d51601b59521
Andrew Graham <andrew@firehawkvfx.com>
2021-03-22 14:34:58 +1030

    update output names

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ data "aws_internet_gateway" "gw" {
 
 data "aws_subnet_ids" "public" {
   vpc_id = data.aws_vpc.primary.id
-  tags = merge( local.common_tags, { "area" : "public" } )
+  tags   = merge(local.common_tags, { "area" : "public" })
 }
 
 data "aws_subnet" "public" {
@@ -35,7 +35,7 @@ data "aws_subnet" "public" {
 
 data "aws_subnet_ids" "private" {
   vpc_id = data.aws_vpc.primary.id
-  tags = merge( local.common_tags, { "area" : "private" } )
+  tags   = merge(local.common_tags, { "area" : "private" })
 }
 
 data "aws_subnet" "private" {
@@ -45,12 +45,12 @@ data "aws_subnet" "private" {
 
 data "aws_route_tables" "public" {
   vpc_id = data.aws_vpc.primary.id
-  tags = merge( local.common_tags, { "area" : "public" } )
+  tags   = merge(local.common_tags, { "area" : "public" })
 }
 
 data "aws_route_tables" "private" {
   vpc_id = data.aws_vpc.primary.id
-  tags = merge( local.common_tags, { "area" : "private" } )
+  tags   = merge(local.common_tags, { "area" : "private" })
 }
 
 data "vault_generic_secret" "private_domain" { # Get the map of data at the path
@@ -79,35 +79,17 @@ locals {
   private_subnets            = sort(data.aws_subnet_ids.private.ids)
   private_subnet_cidr_blocks = [for s in data.aws_subnet.private : s.cidr_block]
   private_domain             = lookup(data.vault_generic_secret.private_domain.data, "value")
-
   vpn_cidr                   = var.vpn_cidr
   onsite_private_subnet_cidr = var.onsite_private_subnet_cidr
-
-  onsite_public_ip        = var.onsite_public_ip
-  private_route_table_ids = sort(data.aws_route_tables.private.ids)
-  public_route_table_ids  = sort(data.aws_route_tables.public.ids)
-  public_domain_name      = "none"
-  route_zone_id           = "none"
+  onsite_public_ip           = var.onsite_public_ip
+  private_route_table_ids    = sort(data.aws_route_tables.private.ids)
+  public_route_table_ids     = sort(data.aws_route_tables.public.ids)
+  public_domain_name         = "none"
+  route_zone_id              = "none"
+  instance_name              = "${lookup(local.common_tags, "vpcname", "default")}_openvpn_ec2_pipeid${lookup(local.common_tags, "pipelineid", "0")}"
   # openvpn_user_pw            = lookup(data.vault_generic_secret.openvpn_user_pw.data, "value")
   # openvpn_admin_pw           = lookup(data.vault_generic_secret.openvpn_admin_pw.data, "value")
 }
-
-output "private_route_table_ids" {
-  value = local.private_route_table_ids
-}
-
-output "public_route_table_ids" {
-  value = local.public_route_table_ids
-}
-
-output "private_ip" {
-  value = module.vpn.private_ip
-}
-
-output "public_ip" {
-  value = module.vpn.public_ip
-}
-
 data "terraform_remote_state" "openvpn_profile" { # read the arn with data.terraform_remote_state.packer_profile.outputs.instance_role_arn, or read the profile name with data.terraform_remote_state.packer_profile.outputs.instance_profile_name
   backend = "s3"
   config = {
@@ -118,17 +100,14 @@ data "terraform_remote_state" "openvpn_profile" { # read the arn with data.terra
 }
 
 module "vpn" {
-  source     = "./modules/tf_aws_openvpn"
-  create_vpn = true
-
-  # example_role_name = "vpn-server-vault-role" # this is to authenticate with the profile
+  source                    = "./modules/tf_aws_openvpn"
+  create_vpn                = true
   example_role_name         = "vpn-server-vault-role" # this authenticates with a dynamically generated secret key
-  name                      = "${lookup(local.common_tags, "vpcname", "default")}_openvpn_ec2_pipeid${lookup(local.common_tags, "pipelineid", "0")}"
+  name                      = local.instance_name
   ami                       = var.openvpn_server_ami
   iam_instance_profile_name = data.terraform_remote_state.openvpn_profile.outputs.instance_profile_name
   resourcetier              = var.resourcetier
   conflictkey               = var.conflictkey
-
   # VPC Inputs
   vpc_id                     = local.vpc_id
   vpc_cidr                   = local.vpc_cidr
@@ -143,33 +122,24 @@ module "vpn" {
   igw_id                     = local.aws_internet_gateway
   public_subnets             = local.public_subnet_cidr_blocks
   private_subnets            = local.private_subnet_cidr_blocks
-
   # EC2 Inputs
-  aws_key_name = var.aws_key_name # This should be replaced with an admin level ssh cert.
-  # private_key          = local.private_key
-  # aws_private_key_path = var.aws_private_key_path
+  aws_key_name  = var.aws_key_name # This should be replaced with an admin level ssh cert.
   instance_type = var.instance_type
-
   # Network Routing Inputs.  source destination checks are disable for nat gateways or routing on an instance.
   source_dest_check = false
-
   # DNS Inputs
   consul_cluster_name    = var.consul_cluster_name
   consul_cluster_tag_key = var.consul_cluster_tag_key
   public_domain_name     = local.public_domain_name
   private_domain_name    = local.private_domain
   route_zone_id          = local.route_zone_id
-
   # # OpenVPN Inputs
   openvpn_user       = "openvpnas"
   openvpn_admin_user = "openvpnas"
-
   # bastion_ip               = module.bastion.public_ip # the vpn is provisioned by the bastion entry point
   # bastion_dependency       = module.bastion.bastion_dependency
   # firehawk_init_dependency = var.firehawk_init_dependency
-
   #sleep will stop instances to save cost during idle time.
-  sleep = var.sleep
-
+  sleep       = var.sleep
   common_tags = local.common_tags
 }
